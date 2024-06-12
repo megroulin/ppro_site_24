b4fb40f29ef47f4f746c0fac9f8bcdef6242a690
megroulin <122873484+megroulin@users.noreply.github.com>
2024-06-12 14:49:55 +0200

    header

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+var burger = document.querySelector(".burger");
+var menu = document.querySelector(".menu");
+
+var toggleMenu = function toggleMenu() {
+  menu.classList.toggle("is-active");
+};
+
+burger.addEventListener("click", toggleMenu);
